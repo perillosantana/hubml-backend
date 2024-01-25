9c22e90c9591d265d01a2f9a27d8664c3b44e43c
Perillo Santana <perillooliveira@gmail.com>
2024-01-25 18:19:35 -0300

    .

--- src/http/routes/sign-in.ts
+++ src/http/routes/sign-in.ts
@@ -3,43 +3,59 @@ import { authentication } from '../authentication'
 import { db } from '@/db/connection'
 import { HumanizedError } from './errors/humanized-error'
 
-export const signIn = new Elysia().use(authentication).post(
-  '/auth/sign-in',
-  async ({ signIn, body, set }) => {
-    const user = await db.query.users.findFirst({
-      where(fields, { eq }) {
-        return eq(fields.login, body.login)
-      },
-    })
-
-    if (!user || !user.password) {
-      set.status = 400
-
-      throw new HumanizedError({
-        status: 'error',
-        message: 'Usuário não existe.',
-      })
+export const signIn = new Elysia()
+  .use(authentication)
+  .options('/*', ({ set }) => {
+    // This is important*
+    // https://github.com/elysiajs/elysia-cors/issues/8
+    set.status = 'OK'
+  })
+  .onBeforeHandle(({ set }) => {
+    // onBeforeHandle will apply these headers on every endpoint
+    set.headers = {
+      // Only allow the domain of FE
+      'Access-Control-Allow-Origin': 'https://hubml.com.br',
+      // This one is needed to pass cookies to client
+      'Access-Control-Allow-Credentials': 'true',
     }
-
-    const isMatch = await Bun.password.verify(body.password, user.password)
-
-    if (user && user.login && isMatch) {
-      return await signIn({
-        login: user.login,
+  })
+  .post(
+    '/auth/sign-in',
+    async ({ signIn, body, set }) => {
+      const user = await db.query.users.findFirst({
+        where(fields, { eq }) {
+          return eq(fields.login, body.login)
+        },
       })
-    } else {
-      set.status = 400
 
-      throw new HumanizedError({
-        status: 'error',
-        message: 'Senha incorreta',
-      })
-    }
-  },
-  {
-    body: t.Object({
-      login: t.String(),
-      password: t.String(),
-    }),
-  },
-)
+      if (!user || !user.password) {
+        set.status = 400
+
+        throw new HumanizedError({
+          status: 'error',
+          message: 'Usuário não existe.',
+        })
+      }
+
+      const isMatch = await Bun.password.verify(body.password, user.password)
+
+      if (user && user.login && isMatch) {
+        return await signIn({
+          login: user.login,
+        })
+      } else {
+        set.status = 400
+
+        throw new HumanizedError({
+          status: 'error',
+          message: 'Senha incorreta',
+        })
+      }
+    },
+    {
+      body: t.Object({
+        login: t.String(),
+        password: t.String(),
+      }),
+    },
+  )
